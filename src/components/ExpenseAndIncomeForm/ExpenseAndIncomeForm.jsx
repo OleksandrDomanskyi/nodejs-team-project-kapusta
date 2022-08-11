c3a1e02fdff1bd398723bed462c1555980d167c4
AnthonyUPTN <tagirovantn@gmail.com>
2022-08-11 17:20:24 +0300

    intermidiate upd

--- src/components/ExpenseAndIncomeForm/ExpenseAndIncomeForm.jsx
+++ src/components/ExpenseAndIncomeForm/ExpenseAndIncomeForm.jsx
@@ -1,12 +1,50 @@
 import { useState } from "react";
+import Select from "react-select";
 
 import s from "./exspenseAndIncomeForm.module.scss";
+
+import { options } from "./options";
 
 const ExpenseAndIncomeForm = () => {
   const [descr, setDescr] = useState("");
 
   const handleChange = (e) => {
     setDescr(e.target.value);
+  };
+
+  const customStyles = {
+    option: (provided, state) => ({
+      ...provided,
+      backgroundColor: state.isSelected ? "#C7CCDC" : "#F5F6FB",
+      fontWeight: 400,
+      borderBottom: "1px solid #eee",
+    }),
+    control: () => ({
+      width: "280px",
+      display: "flex",
+      justifyContent: "space-between",
+      border: "2px solid white",
+      borderTop: "none",
+      borderBottomLeftRadius: "22px",
+      borderBottomRightRadius: "22px",
+      padding: "2px 20px",
+    }),
+    placeholder: () => ({
+      color: "#C7CCDC",
+    }),
+    valueContainer: () => ({
+      display: "flex",
+      alignItems: "center",
+      color: "#C7CCDC",
+    }),
+    indicatorSeparator: () => ({
+      display: "none",
+    }),
+    singleValue: () => ({
+      color: "#52555F",
+      fontWeight: 700,
+      fontSize: 12,
+    }),
   };
   return (
     <form className={s.form}>
@@ -19,9 +57,14 @@ const ExpenseAndIncomeForm = () => {
         minLength={1}
         requared="true"
       />
-      <select className={s.select} name="category">
-        <option value="2121212121">2121212</option>
-      </select>
+      <Select
+        className={s.select}
+        options={options}
+        name="category"
+        placeholder="Product category"
+        styles={customStyles}
+        required
+      />
       <input className={s.sum} name="sum" placeholder="00.00UAH" />
       <div className={s.btnWrapper}>
         <button className={s.inputBtn} type="submit">
