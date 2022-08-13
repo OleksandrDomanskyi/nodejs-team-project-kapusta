73e2516ab7b1e4c73503ced42afa77e1903c14db
MawkaA <94409557+MawkaA@users.noreply.github.com>
2022-08-13 00:16:53 -0400

    updates
    1  добавлена иконка для кнопки назад
    
    2 установлена библиотека   date-fns
    
    3 создан компонент mainBtn для стрелки назад на странице  репортс
    
    4 Добавлен компонент Balance на страницу репорт так как он одинаковый с главной
    
    5 добавлен компонент headerReport для отображения  общей суммы доходов и расходов

---
+++ src/components/ReportsHeader/ReportsHeader.jsx
@@ -0,0 +1,48 @@
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+// import { useSelector } from 'react-redux';
+// import { balanceSelectors } from '../../redux/balance/balance-selectors.js';
+// import balanceOperations from '../../redux/balance/balance-operations';
+
+function numberWithCommas(x) {
+    return parseFloat(x)
+      .toFixed(2)
+      .toString()
+      .replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ' ');
+  }
+
+   //balance logic must be updated 
+  const ReportHeader = () => {
+    const dispatch = useDispatch();
+    const currentExpenses = 0;
+    const currentIncomses = 0;
+   
+    useEffect(() => {
+      dispatch(currentExpenses.updateCurrentExpenses());
+      dispatch(currentIncomses.updateCurrentIncomes());
+    }, [dispatch]);
+  
+    return (
+      <div className="report-header">
+        <div >
+          <p >
+            Expenses:{' '}
+            <span >
+              - {numberWithCommas(currentExpenses) ?? 0} грн
+            </span>
+          </p>
+        </div>
+        <div ></div>
+        <div >
+          <p >
+            Incomes:{' '}
+            <span >
+              + {numberWithCommas(currentIncomses) ?? 0} грн
+            </span>
+          </p>
+        </div>
+      </div>
+    );
+  };
+  
+  export default ReportHeader;
