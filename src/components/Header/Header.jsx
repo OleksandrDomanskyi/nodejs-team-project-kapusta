a0c9762625d4972fe7f4e7fc72923eb68935a0ef
VovaRubakha <97684717+VovaRubakha@users.noreply.github.com>
2022-08-13 18:41:46 +0300

    add header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,31 @@
-import NavBarMenu from "./NavBarMenu/NavBarMenu";
+// import NavBarMenu from "./NavBarMenu/NavBarMenu";
+import logo from "../../images/logo.png"
+import logout from "../../images/logout.png"
+import UserMenu from "./UserMenu";
+
+import styles from "./header.module.scss"
 
 const Header = () => {
-
+  const isLogin = true;
     return (
-        <header>
-            <NavBarMenu />
+        <header >
+            <div className={styles.headerContainer}>
+              <div className={styles.headerLogo}>
+                <img src={logo} alt="logo"/>
+            </div> 
+              <div className={styles.userBlock}>
+                  {isLogin && (
+                    <>
+                      <UserMenu />
+                      <button type="button" className={styles.logoutBtn_mobile}>
+                       <img src={logout} alt="logout"/>
+                      </button>
+                      <button type="button" className={styles.logoutBtn}>Exit</button>
+                    </>
+                  )}
+              </div>
+            </div>
+            {/* <NavBarMenu /> */}
         </header>
     );
 };
