d62b9d882ddd3b4fb884ecf76a3de8fe3579e185
Matvii Skrypka <matviyskrypka@gmail.com>
2022-08-15 21:31:06 +0300

    initial

--- src/components/AuthForm/AuthForm.jsx
+++ src/components/AuthForm/AuthForm.jsx
@@ -1,8 +1,44 @@
-const AuthForm = () => {
 
-    return (
-        <></>
-    );
-}
+import { useState, } from 'react';
+import s from "./auth-form.module.scss"
 
-export default AuthForm;
+
+export default function AuthForm({ onSubmit, onSubmit2 }) {
+    const [data, setData] = useState({
+        email: "",
+        password: ""
+    });
+    
+    const isEmailValid = data.email.match(/[A-Za-z0-9]+@[A-Za-z0-9]+\.[A-Za-z]+/i);
+    const isPasswordValid = data.password.length >= 6;
+    const handleChange = ({ target }) => {
+        
+        const { name, value } = target;
+        
+        setData(prevForm => ({
+            ...prevForm,
+            [name]: value,
+        }));
+    };
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if (e.target==="log"){
+        const { email, password  } = data;
+        onSubmit({ email, password })}else{            
+        const { email, password  } = data;
+        onSubmit2({ email, password })
+        }
+    };
+    return (<form className={s.form} onSubmit={handleSubmit} action="">
+    <label className={s.label} htmlFor="email">Email:</label>
+    <input className={s.input} value={data.email}  onChange={handleChange} name="email" type="email" id="email" placeholder="your@email.com" pattern="[A-Za-zА-Яа-яЁёЄєЇї0-9._%+-]+@[A-Za-zА-Яа-яЁёЄєЇї0-9.-]+\.[A-Za-zА-Яа-яЁёЄєЇї]{2,4}$" />
+    <label className={s.label} htmlFor="password">Password:</label>
+    <input className={s.input} value={data.password} onChange={handleChange} name="password" type="password" id="password" placeholder="Password" />
+    <div className={s.buttons}>
+    <button type='submit' name='log' className={s.login} disabled={!isEmailValid || !isPasswordValid || data.name?.length < 1}>LOG IN</button>
+    <button type='submit' name="reg" className={s.reg} disabled={!isEmailValid || !isPasswordValid || data.name?.length < 1}>REGISTRATION</button>         
+    </div>    
+</form>
+    )
+};
+
--- src/pages/AuthPage/AuthPage.jsx
+++ src/pages/AuthPage/AuthPage.jsx
@@ -1,10 +1,36 @@
-import AuthForm from "../../components/AuthForm";
+import { useState } from "react";
 
+import AuthForm from "../../components/AuthForm/AuthForm";
+import BgWrapper from "../../components/BgWrapper";
+
+import title from "../../images/title.svg"
+import s from "./auth-page.module.scss"
 const AuthPage = () => {
+const login=()=>{
 
-    return (
-        <AuthForm />
-    );
+};
+const reg=()=>{
+
 }
+  return (
+    <>
+    <BgWrapper>
+        <section className={s.section}>
+            
+          <div className={s.container}>
+            <h1 className={s.title}><img className={s.image} src={title} alt="" />Smart Finance</h1>
+            <div className={s.wrapper}>
+                <span className={s.text}>You can log in with your Google Account:</span>
+                <button className={s.google}>Google</button>
+                <span className={s.registration_text}>Or log in using an email and password, after registering:</span>
+                <AuthForm onSubmit={login} onSubmit2={reg}/>
+                
+            </div>
+          </div>
+        </section>
+        </BgWrapper>
+    </>
+  );
+};
 
 export default AuthPage
