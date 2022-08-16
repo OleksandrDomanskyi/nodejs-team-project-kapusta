70c6cf40f22a11f4a83d0a7346685bfb1e7d47d0
AnthonyUPTN <tagirovantn@gmail.com>
2022-08-16 19:04:26 +0300

    add balanceForm component

--- src/components/Balance/Balance.jsx
+++ src/components/Balance/Balance.jsx
@@ -1,45 +1,22 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
-import NumberFormat from "react-number-format";
-import { useMediaQuery } from "react-responsive";
 
 import iconsSprite from "../../images/icons.svg";
 import s from "./balance.module.scss";
 
-import Calendar from "../Calendar";
+import BalanceForm from "./BalanceForm";
 import BalanceModal from "./BalanceModal";
 import BalanceFormModal from "./BalanceFormModal";
 
-import { setUserBalance } from "../../shared/services/auth";
-
 const Balance = ({ type }) => {
-  const [balance, setBalance] = useState("1");
-  const [startDate, setStartDate] = useState(new Date());
+  const [balance, setBalance] = useState("");
   const [modalOpen, setModalOpen] = useState(false);
-
-  const isMobile = useMediaQuery({ query: "(max-width: 767px)" });
-  const handleDateChange = (date) => setStartDate(date);
-
-  const handleChange = (e) => {
-    setBalance(e.target.value);
-  };
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-
-    if (balance) {
-      const newBalance = await setUserBalance(
-        parseFloat(balance.split(" ").join(""))
-      );
-      console.log(newBalance);
-    }
-  };
 
   const openModal = () => {
     setModalOpen((prev) => !prev);
     document.body.style.overflow = "hidden";
   };
-  console.log(type);
+
   return (
     <>
       {modalOpen && (
@@ -58,41 +35,7 @@ const Balance = ({ type }) => {
             <use href={`${iconsSprite}#icon-reports`}></use>
           </svg>
         </Link>
-        <div className={s.formWrapper}>
-          <span className={s.balance}>Balanсe:</span>
-          <form className={s.form} onSubmit={handleSubmit}>
-            <label htmlFor="balance">
-              <NumberFormat
-                className={s.input}
-                name="balance"
-                type="text"
-                value={balance}
-                onChange={handleChange}
-                thousandSeparator=" "
-                decimalSeparator="."
-                decimalScale={2}
-                fixedDecimalScale={true}
-                suffix=" UAH"
-                placeholder="00.00 UAH"
-                minLength={1}
-              />
-            </label>
-            <button className={s.btn} type="submit">
-              Confirm
-            </button>
-          </form>
-          {isMobile && (
-            <div className={s.mobileCalend}>
-              <button className={s.addBtn} onClick={openModal}>
-                Add transaction
-              </button>
-              <Calendar
-                startDate={startDate}
-                onHandleChange={handleDateChange}
-              />
-            </div>
-          )}
-        </div>
+        <BalanceForm openModal={openModal} setMainBalance={setBalance} />
       </div>
     </>
   );
