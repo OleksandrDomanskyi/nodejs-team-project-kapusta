3fc7a34a56c1b6678ae783b92534455a3e353d54
Oleksandr Domanskiy <domanskyioleksandr@gmail.com>
2022-08-17 21:04:09 +0300

    add graphic package

--- src/components/Report/ReportNavigationBar/CurrentPeriodItem/CurrentPeriodItem.jsx
+++ src/components/Report/ReportNavigationBar/CurrentPeriodItem/CurrentPeriodItem.jsx
@@ -1,11 +1,12 @@
 import iconsSprite from "../../../../images/icons.svg";
+import styles from "./current-period-item.module.scss";
 
 const CurrentPeriodItem = () => {
 
     return (
         <div>
             <p>Current period:</p>
-            <div>
+            <div className={styles.option}>
                 <button type="button">
                     <svg width="4" height="10">
                         <use href={`${iconsSprite}#icon-arrow-left`}></use>
