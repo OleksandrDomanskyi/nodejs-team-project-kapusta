03c12ed24b81c7672de28c9687ed093043b816fd
Oleg Muraviov <o.muraviov2014@gmail.com>
2022-08-18 17:36:58 +0300

    fixed logic summary

--- src/blocks/Summary/Summary.jsx
+++ src/blocks/Summary/Summary.jsx
@@ -11,7 +11,7 @@ const Summary = ({ type }) => {
     (item) => item.type === type
   );
 
-  const summaryArray = [...initialState];
+  const summaryArray = JSON.parse(JSON.stringify(initialState));
 
   typedTransactions.forEach((transaction) => {
     summaryArray.forEach((month) => {
@@ -21,19 +21,20 @@ const Summary = ({ type }) => {
     });
   });
 
-  console.log("summaryArray", summaryArray);
-  console.log("initialState", initialState);
+  const reversedArray = summaryArray.reverse();
 
   return (
     <div className={s.summary__wrap}>
       <h4 className={s.summary__title}>SUMMARY</h4>
       <ul className={s.summary__list}>
-        {testData.map((item) => (
-          <li key={item.monce} className={s.summary__item}>
-            <p>{item.monce.toUpperCase()}</p>
-            <p>{item.value.toFixed(2)}</p>
-          </li>
-        ))}
+        {reversedArray
+          .filter((item) => item.value !== 0)
+          .map((item) => (
+            <li key={item.id} className={s.summary__item}>
+              <p>{item.monthTitle.toUpperCase()}</p>
+              <p>{item.value.toFixed(2)}</p>
+            </li>
+          ))}
       </ul>
     </div>
   );
